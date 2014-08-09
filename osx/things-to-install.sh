d33ac98b7ed38b17c2ecf12a87069311ef279813
Matt Sweeney <mattswe@gmail.com>
2014-08-09 07:44:00 -0700

    Add comments

--- osx/things-to-install.sh
+++ osx/things-to-install.sh
@@ -1,7 +1,10 @@
 # list of things to to install from homebrew
 
-brew install reattach-to-user-namespace
+# tmux and friends
 brew install tmux
+brew install reattach-to-user-namespace
+
+# utils
 brew install tig
 brew install htop
 brew install vit
